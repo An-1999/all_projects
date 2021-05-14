780ca16e230b74895c70e1ec1ef08179881bf8e4
An-1999 <akhachatryan897@gmail.com>
2021-05-14 22:42:19 +0400

    ah

---
+++ p.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#define ASPECT_RATIO 1.653
+class A
+{
+    private:
+        int age;
+};
+
+int main()
+{
+/*   char* const p = "greeting";
+   p[0] = 'a';
+char p1 = 'a';
+ p = &p1;
+    std::cout << ASPECT_RATIO;
+*/
+A a;
+a.age = 7;
+}
